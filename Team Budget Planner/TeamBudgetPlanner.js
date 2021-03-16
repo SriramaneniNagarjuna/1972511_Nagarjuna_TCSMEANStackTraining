04ae2c4256e66973e83bd07b2ddb8dc2c0c55316
Nagarjuna S <nagarjuna7112@gmail.com>
2021-03-15 22:34:10 -0700

    Team Budget Planner

---
+++ Team Budget Planner/TeamBudgetPlanner.js
@@ -0,0 +1,55 @@
+//function display(){
+  //  alert("Welcome to the Finance Overview")
+//}
+
+var details =[];
+var totalbudget;
+totalbudget=parseInt(0);
+function formsubmit(){
+    var data = toadddetails();
+    insertNewRecord(data);
+    details.push(data);
+    total(data);
+    resetData();
+}
+
+
+
+function toadddetails() {
+    var obj = {}    // empty object
+    obj.Name = document.getElementById("Name").value;
+    obj.ClientName = document.getElementById("ClientName").value;
+    obj.Budget = document.getElementById("Budget").value;
+    console.log(obj);
+    return obj; 
+}
+function insertNewRecord(data){
+ var table = document.getElementById("FinanceOverview")
+ var body = table.getElementsByTagName("tbody")[0];
+ var newRow = body.insertRow(body.length);  // row created 
+
+ var cell1 = newRow.insertCell(0);          // cell created 
+ cell1.innerHTML=data.Name;                 // value placed 
+
+ var cell2 = newRow.insertCell(1);          // cell created 
+ cell2.innerHTML=data.ClientName;                 // value placed
+
+ var cell3 =newRow.insertCell(2);
+ cell3.innerHTML=data.Budget;
+
+ 
+}
+
+function resetData() {
+document.getElementById("Name").value="";
+document.getElementById("ClientName").value="";
+document.getElementById("Budget").value="";
+}
+
+function total(data){
+        totalbudget = parseInt(data.Budget) + parseInt(totalbudget);
+        document.getElementById("total").innerHTML=totalbudget;
+}
+
+
+
